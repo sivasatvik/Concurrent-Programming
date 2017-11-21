7a2f5bf581a178b49ae9748fd5f5ccb8069648ce
proton0001 <cs15btech11029@iith.ac.in>
2017-11-22 00:06:32 +0530

    Parallel brute

--- brute/main.cpp
+++ brute/main.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <atomic>
+#include <iostream>
+#include <thread>
 #include <vector>
 #include <random>
 #include <cstdlib>
@@ -80,45 +82,53 @@ void generatesGraph()
     }
 }
 
+int mins[num_proc];
+std::atomic_int vert_no;
 
 // implementation of traveling Salesman Problem
-void travllingSalesmanProblem(int s)
+void travllingSalesmanProblem(int id)
 {
     // store all vertex apart from source vertex
-    vector<int> vertex;
-    for (int i = 0; i < V; i++)
-        if (i != s)
-            vertex.push_back(i);
- 
-    // store minimum weight Hamiltonian Cycle.
-    int min_path = 999999;
-    do {
- 
-        // store current Path weight(cost)
-        int current_pathweight = 0;
-         
-        // compute current path weight
-        int k = s;
-        for (int i = 0; i < vertex.size(); i++) {
-            current_pathweight += graph[k][vertex[i]];
-            // cout<<current_pathweight<<endl;
-			k = vertex[i];
-        }
-        current_pathweight += graph[k][s];
+    do
+    {
+        int sec_vert = vert_no++;   
+        vector<int> vertex;
+        for (int i = 1; i < V; i++)
+            if (i != sec_vert)
+                vertex.push_back(i);
 
- 
-        // update minimum
-        min_path = min(min_path, current_pathweight);
-        
-    } while (next_permutation(vertex.begin(), vertex.end()));
- 
-    cout<< min_path;
+        // store minimum weight Hamiltonian Cycle.
+        int min_path = 999999;
+        do {     
+            // store current Path weight(cost)
+            int current_pathweight = graph[0][sec_vert];
+             
+            // compute current path weight
+            int k = sec_vert;
+            for (int i = 0; i < vertex.size(); i++) {
+                current_pathweight += graph[k][vertex[i]];
+                // cout<<current_pathweight<<endl;
+    			k = vertex[i];
+            }
+            current_pathweight += graph[k][0];
+
+     
+            // update minimum
+            min_path = min(min_path, current_pathweight);
+            // for(int i: vertex)
+                // cout << i << " ";
+        } while (next_permutation(vertex.begin(), vertex.end()));
+     
+        mins[id] = min_path;
+    }while(vert_no < V);
+
 }
  
 // driver program to test above function
 int main()
 {	
 	srand(SEED);
+    vert_no = 1;
     // matrix representation of graph
     generatesGraph();
 	// int graph[][V] = { { 0, 10, 15, 20 },
@@ -126,7 +136,7 @@ int main()
  //                       { 15, 35, 0, 30 },
  //                       { 20, 25, 30, 0 } };
     printGraph();
-
+    cout << "\n\n\n\n";
 
     struct timeval start;
 	gettimeofday(&start, 0);
@@ -137,8 +147,17 @@ int main()
 		threads.push_back(std::thread(travllingSalesmanProblem, i));
 	}
 
-	for(auto &th : threads) th.join();
-
+	for(auto &th : threads)
+        th.join();
+    int cur_min = mins[0];
+    for(int i=1;i<num_proc;i++)
+    {
+        if(mins[i] < cur_min)
+        {
+            cur_min = mins[i];
+        }
+    }
+    cout << cur_min <<endl;
    	struct timeval end;
 	gettimeofday(&end, 0);
 	long long duration = (end.tv_sec-start.tv_sec)*1000000LL + end.tv_usec-start.tv_usec;
